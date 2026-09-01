c5b8c236417c94446fd6eabd5caec88705967723
Administrator <Administrator@DESKTOP-FG7C68P>
2026-09-01 12:51:40 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicios_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*      Primer ejecicio 
+
+            Console.Write("introduci primer numero:");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("introduci segundo numero:");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int suma = numero1 + numero2;
+            string resultado;
+
+            resultado = "La suma es: " + suma;
+            Console.WriteLine(resultado);
+             */
+
+
+
+
+
+
+
+        }
+    }
+}
